917caac6076e8c972861304fc46a2c0460d91f8e
Christian-Tirado-CODE <tiradoc02@gmail.com>
2021-02-02 10:47:22 -0400

    Using Link inside the Nav Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styles from '../styles/Layout.module.css';
-
-const Layout = ({children}) => {
-    return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                    {children}
-            </main>            
-        </div>
-    );
+import React from "react";
+import styles from "../styles/Layout.module.css";
+import Nav from "./Nav";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
