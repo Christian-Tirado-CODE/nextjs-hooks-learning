099fe123de46751538643839a043f781c7f48049
Christian-Tirado-CODE <tiradoc02@gmail.com>
2021-02-02 18:32:38 -0400

    fetching data with getServerSideProps()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,13 +1,34 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
+import Link from "next/link";
+const Article = ({ article }) => {
+  // const router = useRouter();
+  // const {id} = router.query;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
 
-const Article = () => {
-const router = useRouter();
-const {id} = router.query;
-    return (
-        <div>
-            This is article {id}
-        </div>
-    );
+      <Link href="/articles">Go Back</Link>
+    </>
+  );
+};
+
+// Dynamically generate paths with
+//getServerSideProps: Fetch data at time of request(not at build time - getStaticProps)
+//getStaticPaths: Dynamically generate paths
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+
+  return {
+    props: {
+        article,
+    }
+  };
 };
 
 export default Article;
