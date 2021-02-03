7375d57df8c65bb4383e39e90cd811149b468c91
Christian-Tirado-CODE <tiradoc02@gmail.com>
2021-02-03 16:31:04 -0400

    Deleted  unnecessary files

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Sembrando Sentido - Home Page</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name='keywords' content='web development, programming'/>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+            Sembrando Sentido - Homepage
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          
-        </div>
+        
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        
       </footer>
     </div>
   )
